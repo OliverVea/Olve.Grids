efb70091f3f5615f28f5ba3a0654295e81f651a6
Oliver Vea <oliver.vea@gmail.com>
2024-12-19 14:19:08 +0900

    Added VerbosityLevel enum and Parse method for command-line argument parsing.

---
+++ src/Demo/Commands/VerbosityLevels.cs
@@ -0,0 +1,48 @@
+﻿using OneOf.Types;
+
+namespace Demo.Commands;
+
+public readonly record struct VerbosityLevel(int Value)
+{
+    public static bool operator <(VerbosityLevel left, VerbosityLevel right) => left.Value < right.Value;
+    public static bool operator >(VerbosityLevel left, VerbosityLevel right) => left.Value > right.Value;
+    public static bool operator <=(VerbosityLevel left, VerbosityLevel right) => left.Value <= right.Value;
+    public static bool operator >=(VerbosityLevel left, VerbosityLevel right) => left.Value >= right.Value;
+
+    public static readonly VerbosityLevel Quiet = new(0);
+    public bool IsQuiet => this >= Quiet;
+
+    public static readonly VerbosityLevel Normal = new(1);
+    public bool IsNormal => this >= Normal;
+
+    public static readonly VerbosityLevel Verbose = new(2);
+    public bool IsVerbose => this >= Verbose;
+}
+
+public static class VerbosityLevels
+{
+
+    public static OneOf<VerbosityLevel, Error<string>> Parse(string? value)
+    {
+        if (value is null)
+        {
+            return new Error<string>("The verbosity level must be specified.");
+        }
+
+        switch (value.ToLowerInvariant())
+        {
+            case "quiet":
+            case "q":
+                return VerbosityLevel.Quiet;
+            case "normal":
+            case "n":
+                return VerbosityLevel.Normal;
+            case "verbose":
+            case "v":
+                return VerbosityLevel.Verbose;
+            default:
+                return new Error<string>(
+                    "The verbosity level must be 'Quiet', 'Normal', or 'Verbose' or 'q', 'n', or 'v'.");
+        }
+    }
+}
