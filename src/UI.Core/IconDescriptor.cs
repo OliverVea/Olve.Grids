a685334e9055ee29c7251b1ee2dbed63819e3717
Oliver Vea <oliver.vea@gmail.com>
2025-01-12 12:00:16 +0900

    Refactored UI components for consistency and added functionality.

--- src/UI.Core/IconDescriptor.cs
+++ src/UI.Core/IconDescriptor.cs
@@ -18,6 +18,7 @@ public class IconDescriptor
     public static readonly IconDescriptor Minus = new("fa fa-minus");
     public static readonly IconDescriptor Trash = new("fa fa-trash");
     public static readonly IconDescriptor Edit = new("fa fa-pencil");
+    public static readonly IconDescriptor Check = new("fa fa-check");
     public static readonly IconDescriptor Circle = new("fa fa-circle");
     public static readonly IconDescriptor CircleSolid = new("fa-solid fa-circle");
     public static readonly IconDescriptor XSolid = new("fa-solid fa-xmark");
