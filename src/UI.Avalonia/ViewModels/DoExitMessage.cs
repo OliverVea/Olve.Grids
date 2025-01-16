adc255485355331fcdebadf4dafbb31e96861ba2
Oliver Vea <oliver.vea@gmail.com>
2025-01-16 20:37:07 +0900

    Removed entire UI.Avalonia project

--- src/UI.Avalonia/ViewModels/DoExitMessage.cs
+++
@@ -1,5 +0,0 @@
-﻿using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace UI.Avalonia.ViewModels;
-
-public class DoExitMessage() : ValueChangedMessage<int>(0);
--- src/UI.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,5 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace UI.Avalonia.ViewModels;
-
-public class ViewModelBase : ObservableObject;
