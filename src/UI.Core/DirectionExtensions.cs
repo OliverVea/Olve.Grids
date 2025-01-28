ebe9aabe5fb01457d56ffc61c27398bd342ac9d3
Oliver Vea <oliver.vea@gmail.com>
2025-01-28 16:50:45 +0900

    refactor: added `Direction.ToChevron` extension method

---
+++ src/UI.Core/DirectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Olve.Grids.Primitives;
+
+namespace UI.Core;
+
+public static class DirectionExtensions
+{
+    public static IconDescriptor ToChevron(this Direction direction) => direction switch
+    {
+        Direction.Down => IconDescriptor.ChevronDown,
+        Direction.Up => IconDescriptor.ChevronUp,
+        Direction.Left => IconDescriptor.ChevronLeft,
+        Direction.Right => IconDescriptor.ChevronRight,
+        _ => throw new ArgumentOutOfRangeException(nameof(Direction), direction, null),
+    };
+}
