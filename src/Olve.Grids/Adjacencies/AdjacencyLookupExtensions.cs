c8585e400e31a81cdf43e683e6c1770e5c33b220
Oliver Vea <oliver.vea@gmail.com>
2025-01-28 15:53:38 +0900

    feat: added `Toggle` to `this IAdjacencyLookup` to toggle if a neighbor is in a direction with a method

---
+++ src/Olve.Grids/Adjacencies/AdjacencyLookupExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Olve.Grids.Grids;
+using Olve.Grids.Primitives;
+
+namespace Olve.Grids.Adjacencies;
+
+public static class AdjacencyLookupExtensions
+{
+    public static bool Toggle(this IAdjacencyLookup adjacencyLookup, TileIndex from, TileIndex to, Direction direction)
+    {
+        var currentDirection = adjacencyLookup.Get(from, to);
+        if (currentDirection.HasFlag(direction))
+        {
+            adjacencyLookup.Remove(from, to, currentDirection);
+            return false;
+        }
+
+        adjacencyLookup.Set(from, to, currentDirection | direction);
+        return true;
+    }
+}
