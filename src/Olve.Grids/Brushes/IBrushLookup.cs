cd928f29b9ee412f09c015179914e9822e33bbab
Oliver Vea <oliver.vea@gmail.com>
2025-01-30 18:58:38 +0900

    docs: adding xml comments to `IBrushLookup` and `IReadOnlyBrushLookup`

--- src/Olve.Grids/Brushes/IBrushLookup.cs
+++ src/Olve.Grids/Brushes/IBrushLookup.cs
@@ -5,15 +5,40 @@ namespace Olve.Grids.Brushes;
 
 public interface IBrushLookup : IReadOnlyBrushLookup
 {
+    /// <summary>
+    /// Set the brushes for a tile
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
+    /// <param name="cornerBrushes">The brushes for the tile</param>
     void SetCornerBrushes(TileIndex tileIndex, CornerBrushes cornerBrushes);
 
+    /// <summary>
+    /// Set the brush id for a tile and corner
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
+    /// <param name="corner">The corner from the perspective of the tile</param>
+    /// <param name="brushId">The brush id</param>
     void SetCornerBrush(
         TileIndex tileIndex,
         Corner corner,
         BrushIdOrAny brushId
     );
 
+    /// <summary>
+    /// Clear all brushes
+    /// </summary>
     void Clear();
+
+    /// <summary>
+    /// Clear all brushes for a tile
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
     void ClearTileBrushes(TileIndex tileIndex);
+
+    /// <summary>
+    /// Clear the brush id for a tile and corner
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
+    /// <param name="corner">The corner from the perspective of the tile</param>
     void ClearTileBrush(TileIndex tileIndex, Corner corner);
 }
--- src/Olve.Grids/Brushes/IReadOnlyBrushLookup.cs
+++ src/Olve.Grids/Brushes/IReadOnlyBrushLookup.cs
@@ -5,10 +5,37 @@ namespace Olve.Grids.Brushes;
 
 public interface IReadOnlyBrushLookup
 {
+    /// <summary>
+    /// Get the brush ids in the lookup
+    /// </summary>
     IEnumerable<BrushId> Brushes { get; }
 
+    /// <summary>
+    /// Get the entries in the lookup
+    /// </summary>
     IEnumerable<(TileIndex TileIndex, Corner Corner, BrushId Brush)> Entries { get; }
+
+    /// <summary>
+    /// Get the brushes for a tile
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
+    /// <returns>The brushes for the tile or NotFound if no brushes are found</returns>
     OneOf<CornerBrushes, NotFound> GetBrushes(TileIndex tileIndex);
+
+    /// <summary>
+    /// Get the brush id for a tile and corner
+    /// </summary>
+    /// <param name="tileIndex">The tile index</param>
+    /// <param name="corner">The corner from the perspective of the tile</param>
+    /// <returns>The brush id or NotFound if no brush id is found</returns>
     OneOf<BrushId, NotFound> GetBrushId(TileIndex tileIndex, Corner corner);
+
+    /// <summary>
+    /// Get the tiles that have a brush id set for the corner
+    /// </summary>
+    /// <param name="brushId">The brush id</param>
+    /// <param name="corner">The corner <b>from the perspective of the brush</b></param>
+    /// <returns>The tiles that have the brush id set for the corner or NotFound if no tiles are found</returns>
+    /// <remarks>Please note that the corner is from the perspective of the brush, i.e., the opposite corner of the tile</remarks>
     OneOf<IReadOnlySet<TileIndex>, NotFound> GetTiles(BrushId brushId, Corner corner);
 }
