7b45c2113ce98df822d5d1d5eb5f89dc7be9220d
Oliver Vea <oliver.vea@gmail.com>
2025-01-30 19:04:20 +0900

    refactor: add `Directions.All` for all possible directions
    Unlike `Directions.Cardinal`, `Directions.All` includes all possible values, including composite directions, e.g., `up | down`. This is dynamically generated based on `Direction.All`.

--- src/Olve.Grids/Primitives/Directions.cs
+++ src/Olve.Grids/Primitives/Directions.cs
@@ -4,4 +4,9 @@ public static class Directions
 {
     public static readonly IReadOnlyList<Direction>
         Cardinal = [ Direction.Up, Direction.Down, Direction.Left, Direction.Right, ];
+
+    public static readonly IReadOnlyList<Direction> All = Enumerable
+        .Range(1, (int)Direction.All)
+        .Select(x => (Direction)x)
+        .ToArray();
 }
